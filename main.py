3bd2eba1a1f3822710945742c4e75e755ebfc21c
Alex Zieba <alex@zieba.net>
2024-02-20 13:09:25 -0600

    Added method to decline popups and navigate to profile page, need to begin the follower counting process...

--- main.py
+++ main.py
@@ -14,20 +14,59 @@ def login(driver: webdriver.Chrome, username:str, password:str) -> bool:
     driver.find_element(By.NAME, "password").send_keys(u'\ue007')
     print("Login attempted")
     driver.implicitly_wait(15)#wait on login
-    if driver.current_url == ("https://www.instagram.com/accounts/onetap/?next=%2F"):
-        declineBullshit(driver)
+    declineBullshit(driver)
     return driver.current_url == "https://www.instagram.com/"
 
 def declineBullshit(driver: webdriver.Chrome) -> bool:
-     #HERE
+     driver.implicitly_wait(15)
+     #time.sleep(10)
+     print("DECLINING BULLSHIT")
+     driver.find_element(By.XPATH,"//div[text()='Not now']").send_keys(u'\ue007') #Save Your Login info?
+     #time.sleep(15)
+     driver.implicitly_wait(10)
+     print("DECLINING MORE BS")
+     driver.find_element(By.XPATH, "//button[text()='Not Now']").send_keys(u'\ue007') #Turn on notifications?
+     print("BULLSHIT DECLINED!")
+
+
      return True
 
 
 
 def navigateToProf(driver: webdriver.Chrome, username) -> bool:
-    driver.implicitly_wait(5) #web driver waits before trying that s%t
-    driver.find_element(By.LINK_TEXT, "/" + username + "/").click()
-    return (driver.current_url == "https://www.instagram.com/"+ username +"/")
+    driver.implicitly_wait(15) #web driver waits before trying that s%t
+    print("NAVIGATING")
+    href:str = "//a[@href='/" + username + "/']"
+    driver.find_element(By.XPATH, href).send_keys(u'\ue007')
+    driver.implicitly_wait(15)
+    return (driver.current_url == ("https://www.instagram.com/" + username + "/"))
+
+
+#TODO: COUNT_ALL
+def count_all(driver:webdriver.Chrome, username:str):
+    followers = None
+    following = None
+    if (driver.current_url == ("https://www.instagram.com/" + username + "/")):
+        followers = get_followers(driver)
+    if (driver.current_url == ("https://www.instagram.com/" + username + "/")):
+        following = get_following(driver)
+    if (followers == None | following == None):
+        return False
+    else:
+        return True
+
+#TODO: GET_FOLLOWERS
+def get_followers(driver: webdriver.Chrome):
+    followers = []
+    driver.find_elements()
+    return followers
+
+#TODO: GET_FOLLOWING
+def get_following(driver: webdriver.Chrome):
+    following = []
+
+    return following
+
 
 def __main__():
     print("Enter Username: ")
@@ -39,12 +78,13 @@ def __main__():
     logged = login(driver,username,password)
     if (logged):
         print("Jarvis, we made it into " + username + "'s insta...")
+    print("MADE IT TO FEED")
     navd = navigateToProf(driver, username)
     if (navd):
-        print("Jarvis, we made it to the profile page")
-
-    
-    time.sleep(10)
+        print("MADE IT TO PROFILE")
+    #count_all(driver, username)
+        
+    print("SUCCESS")
     driver.quit()
     return
 
